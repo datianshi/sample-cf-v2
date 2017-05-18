ccd22fc4c2822fb11da01b3533a4d604d9118b29
datianshi <dsz0111@gmail.com>
2017-05-18 11:16:11 -0500

    add delete bosh script

--- azure/terraform/setup_jumpbox.sh
+++ azure/terraform/setup_jumpbox.sh
@@ -2,10 +2,11 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TSTATE_FILE="${DIR}/terraform.tfstate"
+ENV_FILE="${DIR}/env.sh"
 
 
 JUMPBOX_IP=$(terraform state show -state ${TSTATE_FILE} azurerm_public_ip.jb-public-ip | grep ip_address | head -n 1 | awk '{print $3}')
 
 
-ssh -i ${PRIVATE_KEY_PATH} ubuntu@${JUMPBOX_IP} 'git clone https://github.com/datianshi/sample-cf-v2'
-scp -i ${PRIVATE_KEY_PATH} ${TSTATE_FILE} env.sh ubuntu@${JUMPBOX_IP}:./sample-cf-v2/azure/terraform/
+ssh -i ${PRIVATE_KEY_PATH} ubuntu@${JUMPBOX_IP} 'rm -rf sample-cf-v2 && git clone https://github.com/datianshi/sample-cf-v2'
+scp -i ${PRIVATE_KEY_PATH} ${TSTATE_FILE} ${ENV_FILE} ubuntu@${JUMPBOX_IP}:./sample-cf-v2/azure/terraform/
