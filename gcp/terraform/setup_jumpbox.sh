c6d31f1db75e02a631f12d43f715a51112eb18b1
datianshi <dsz0111@gmail.com>
2017-05-22 15:43:52 -0500

    Fix the gcp director account.json

--- gcp/terraform/setup_jumpbox.sh
+++ gcp/terraform/setup_jumpbox.sh
@@ -3,8 +3,9 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TSTATE_FILE="${DIR}/terraform.tfstate"
 ENV_FILE="${DIR}/env.sh"
+ACCOUNT_FILE="${DIR}/account.json"
 
 
 gcloud compute ssh bosh-bastion --command 'rm -rf sample-cf-v2 && git clone https://github.com/datianshi/sample-cf-v2' --zone $TF_VAR_zone
 gcloud compute ssh bosh-bastion --command 'cd sample-cf-v2 && git submodule init && git submodule update' --zone $TF_VAR_zone
-gcloud compute copy-files "${PRIVATE_KEY_PATH}" ${TSTATE_FILE} account.json env.sh bosh-bastion:~/sample-cf-v2/gcp/terraform/ --zone $TF_VAR_zone
+gcloud compute copy-files "${PRIVATE_KEY_PATH}" ${TSTATE_FILE} ${ACCOUNT_FILE} ${ENV_FILE} bosh-bastion:~/sample-cf-v2/gcp/terraform/ --zone $TF_VAR_zone
