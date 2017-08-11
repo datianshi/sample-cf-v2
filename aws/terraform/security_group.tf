3c694aaacf5662c3af7b0489804afa76fee2d9bb
datianshi <dsz0111@gmail.com>
2017-08-11 10:12:31 -0500

    update bosh deployment

--- aws/terraform/security_group.tf
+++ aws/terraform/security_group.tf
@@ -88,6 +88,12 @@ resource "aws_security_group" "directorSG" {
         cidr_blocks = ["0.0.0.0/0"]
     }
     ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+    ingress {
         from_port = 25555
         to_port = 25555
         protocol = "tcp"
--- aws/terraform/setup_jumpbox.sh
+++ aws/terraform/setup_jumpbox.sh
@@ -8,6 +8,6 @@ ENV_FILE="${DIR}/env.sh"
 JUMPBOX_IP=$(terraform state show -state ${TSTATE_FILE} aws_instance.jumpbox | grep public_ip | tail -n 1 | awk '{print $3}')
 
 
-ssh -i ${PRIVATE_KEY_PATH} ubuntu@${JUMPBOX_IP} 'rm -rf sample-cf-v2 && git clone https://github.com/datianshi/sample-cf-v2'
+ssh -i ${PRIVATE_KEY_PATH} ubuntu@${JUMPBOX_IP} 'rm -rf sample-cf-v2 && git clone -b vpc-peering https://github.com/datianshi/sample-cf-v2'
 ssh -i ${PRIVATE_KEY_PATH} ubuntu@${JUMPBOX_IP} 'cd sample-cf-v2 && git submodule init && git submodule update'
 scp -i ${PRIVATE_KEY_PATH} ${TSTATE_FILE} ${ENV_FILE} ${PRIVATE_KEY_PATH} ubuntu@${JUMPBOX_IP}:./sample-cf-v2/aws/terraform/
